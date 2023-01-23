b6d2cd7b144b5be786c3e8a20c9adf0b451f60a0
dotpyfreak <josespeter12@gmail.com>
2023-01-23 22:38:17 +0100

    Collaborating on printf

--- convert_number.c
+++ convert_number.c
@@ -49,7 +49,7 @@ int print_HEX(va_list ap, params_t *params)
 		l = (unsigned short int)va_arg(ap, unsigned int);
 	else
 		l = (unsigned int)va_arg(ap, unsigned int);
-	
+
 	str = convert(l, 16, CONVERT_UNSIGNED, params);
 	if (params->hashtag_flag && l)
 	{
