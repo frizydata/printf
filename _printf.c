9968aad3373c6647470b6e92ff9a52eef8df5070
dotpyfreak <josespeter12@gmail.com>
2023-01-24 22:12:11 +0100

    some changes from my end

--- _printf.c
+++ _printf.c
@@ -5,13 +5,11 @@ void print_buffer(char buffer[], int *buff_ind);
 
 
 /**
-
  * _printf - Printf function
  *
  * @format: format.
-
+ *
  * Return: Printed chars.
-
  */
 
 int _printf(const char *format, ...)
@@ -96,13 +94,9 @@ int _printf(const char *format, ...)
 
 
 /**
-
  * print_buffer - Prints the contents of the buffer if it exist
-
  * @buffer: Array of chars
-
  * @buff_ind: Index at which to add next char, represents the length.
-
  */
 
 void print_buffer(char buffer[], int *buff_ind)
